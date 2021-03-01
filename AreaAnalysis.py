dd186155da1347fcc5e03e644079b79e46cbcccb
Dennis Huebner <dehuebne@uni-potsdam.de>
2021-03-01 23:51:57 +0100

    calculate the real size of a plant; open the corresponding txt file

--- AreaAnalysis.py
+++ AreaAnalysis.py
@@ -18,7 +18,7 @@ This is a temporary script file.
 #The CCD Camera IMAG-K7 (Allied Vision Technologies) can be used, which features a 1/2" chip with 640 x 480 pixel resolution. 
 #The data are digitized within the camera and transferred via Ethernet interface (GigE-Vision ® ) to the PC.
 
-from skimage.io import imread,imshow,imsave,imshape
+from skimage.io import imread,imshow,imsave
 import tifffile
 from skimage.color import rgb2gray
 from skimage.filters import threshold_otsu
@@ -34,10 +34,16 @@ from skimage.morphology import remove_small_objects,label
 # use key=2 or key =5 for our data
 
 
-img1=imread("/home/dennis/Schreibtisch/Uni_Potsdam/Bioimage/practical/Dennis_GrowthRate/180801/180801_FL-1.tif",key=2) 
+img1=imread("/home/dennis/Schreibtisch/Uni_Potsdam/Bioimage/practical/bioimage/Dennis_GrowthRate/180801",key=1) 
 imshow(img1)
 
-img2=imread("/home/dennis/Schreibtisch/Uni_Potsdam/Bioimage/practical/Dennis_GrowthRate/180802/180802_6.tif",key=1)
+#open corresponding txt file
+img1txt=open("/home/dennis/Schreibtisch/Uni_Potsdam/Bioimage/practical/bioimage/Dennis_GrowthRate/180801/180801_FL-1.txt", 'r')
+content = img1txt.read()
+print (content)
+
+
+img2=imread("/home/dennis/Schreibtisch/Uni_Potsdam/Bioimage/practical/Dennis_GrowthRate/180802/180802_6.tif",key=2)
 imshow(img2)
 
 plant1=[img1,img2]
@@ -56,12 +62,12 @@ import numpy as np
 np.unique(l)
 nb=len(np.unique(l))-1
 
-np.sum(l==0)
+np.sum(l==1)
 sizes = []
 for i in range(1,nb+1):
     sizes.append(np.sum(l==i))
 
-sizes_1 = sizes
+sizes_i = sizes
 
 
 
@@ -78,40 +84,54 @@ imshow(img5)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 # =============================================================================
+# calculate the size of a plant 
+# example calculation of plant 1 in Image 1 (maybe)
+# =============================================================================
+
+# homogenous illumination of an imaged area: 10x13 cm
+# Dimemsnsion of the image
+img1.shape[1] 
+#-> X axis: 640px = 13cm
+
+img1.shape[0]
+#-> Y axis: 480px = 10 cm
+
+img1.shape
+# 480 px * 640px = 307200 px²
+# 10cm *13 cm = 130 cm² 
+# --> 130cm² = 307200 px²
+
+# Calculate size of plant 1
+np.sum(l==1) # 288 pixel of plant 1
+size of plant 1 = (130*288)/307200 = 0,12 cm² = 12 mm² 
+
+#==============================================================================
+
+
+
+
+
+
+
+#==============================================================================
 # Ideas
 # =============================================================================
 #plant1
-for x in 1:len(list)
-        
+
+#for x in 1:len(list)
 
 
-
-
-
-
-for i in 1:2
-    var = sprintf(img+i)    
-    gray = rgb2gray(var)
-    bw = gray > threshold_otsu(gray)
-    l = label(bw)
-    np.unique(l)
-    nb=len(np.unique(l))-1
-    p.sum(l==0)
-    sizes = []
-    for i in range(1,nb+1):
-        sizes.append(np.sum(l==i))
-
-    
-sizes_i = sizes
+#for i in 1:2
+#    var = sprintf(img+i)    
+#    gray = rgb2gray(var)
+#    bw = gray > threshold_otsu(gray)
+#    l = label(bw)
+#    np.unique(l)
+#    nb=len(np.unique(l))-1
+#    p.sum(l==0)
+#    sizes = []
+#    for i in range(1,nb+1):
+#        sizes.append(np.sum(l==i))
+   
+#sizes_i = sizes
