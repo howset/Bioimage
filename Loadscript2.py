4234714ce2cd9fe2c14590bdce44862e9f0af3b2
Dennis Huebner <dehuebne@uni-potsdam.de>
2021-03-28 22:48:59 +0200

    load txt and img files in all directorys

--- Loadscript2.py
+++ Loadscript2.py
@@ -15,11 +15,11 @@ from skimage.filters import threshold_otsu as to
 
 ## Process txt-files
 
-files_txt = glob.glob('/home/dennis/Schreibtisch/Uni_Potsdam/Bioimage/practical/bioimage/Dennis_GrowthRate/180801/*.txt')
+files_txt = glob.glob('/home/dennis/Schreibtisch/Uni_Potsdam/Bioimage/practical/bioimage/Dennis_GrowthRate/*/*.txt')
 files_txt.sort()
 df_txt = pd.DataFrame(columns=('fileRoot','position','plantnumber'))
 
-dict={}
+#dict={}
     
 for file in files_txt:
     print(file) # just to see which file is processed
@@ -36,17 +36,17 @@ for file in files_txt:
     
 #df_txt.sort_values(by=['fileRoot'])
     
-    dict[fileRoot,'topleft'] = txtdf.loc[0,0]
-    dict[fileRoot, 'topcenter'] = txtdf.loc[0,1]
-    dict[fileRoot, 'topright'] = txtdf.loc[0,2]
-    dict[fileRoot, 'bottomleft'] = txtdf.loc[1,0]
-    dict[fileRoot, 'bottomcenter'] = txtdf.loc[1,1]
-    dict[fileRoot, 'bottomright'] = txtdf.loc[1,2]
+   # dict[fileRoot,'topleft'] = txtdf.loc[0,0]
+   # dict[fileRoot, 'topcenter'] = txtdf.loc[0,1]
+   # dict[fileRoot, 'topright'] = txtdf.loc[0,2]
+   # dict[fileRoot, 'bottomleft'] = txtdf.loc[1,0]
+   # dict[fileRoot, 'bottomcenter'] = txtdf.loc[1,1]
+   # dict[fileRoot, 'bottomright'] = txtdf.loc[1,2]
 
 
 ## Process Images
 
-files_img_180801 = glob.glob ('/home/dennis/Schreibtisch/Uni_Potsdam/Bioimage/practical/bioimage/Dennis_GrowthRate/180801/*.tif')
+files_img = glob.glob ('/home/dennis/Schreibtisch/Uni_Potsdam/Bioimage/practical/bioimage/Dennis_GrowthRate/*/*.tif')
 files_img.sort()                      
 df_img = pd.DataFrame(columns=('fileRoot','position','area'))
 for file in files_img:
@@ -66,6 +66,26 @@ for file in files_img:
 ## Merge df_txt and df_img
 df_txt['Area']=df_img['area'] 
 df_merged=df_txt
+
+
+# =============================================================================
+# Matplotlib
+# https://publish.illinois.edu/olaogunbayo/2015/09/29/exponential-functions-ordinary-differential-equations-simulations-2/
+# https://www.epythonguru.com/2020/09/python-exponential-growth-model.html
+# =============================================================================
+
+# Also was du bekommst ist die Fläche der Pflanzen über Zeit. Da die Pflanze exponentiell wächst, 
+# fitten wir die Datenpunkte auf die first order exponential growth equation y(t) = e^(kt). 
+# Dabei ist t der Zeitpunkt und k die Wachstumskonstante. 
+# Die Wachstumskonstante ist quasi das Wachstum pro Zeiteinheit (hier nehmen wir Tage). 
+# --> so which value has k in our case ?? Maybe 7 because we have data points from 7 days?
+
+
+from scipy.integrate import odeint
+from matplotlib import pyplot as plt
+
+
+
 
 
 
